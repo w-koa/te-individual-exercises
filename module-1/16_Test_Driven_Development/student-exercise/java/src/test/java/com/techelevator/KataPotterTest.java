65642ea399787f11ae6aac1a0ca1e52b24f0ebe6
Willis Koa <williskoa@yahoo.com>
2020-02-03 18:46:08 -0500

    Added test for getCost(1)

--- module-1/16_Test_Driven_Development/student-exercise/java/src/test/java/com/techelevator/KataPotterTest.java
+++ module-1/16_Test_Driven_Development/student-exercise/java/src/test/java/com/techelevator/KataPotterTest.java
@@ -7,4 +7,13 @@ import static org.junit.Assert.*;
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class KataPotterTest {
 
+	KataPotter potter = new KataPotter();
+	
+	@Test
+	public void single_purchase_returns_8() {
+		double actual = 8;
+		double expected = potter.getCost([1]);
+		assertEquals(actual, expected);
+	}
+	
 }
