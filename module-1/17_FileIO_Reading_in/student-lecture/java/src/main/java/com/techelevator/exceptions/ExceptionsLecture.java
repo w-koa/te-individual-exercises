9b79b2df67e91cd19a4e47a1c21500d0caa599f7
Margaret Green <margaret@techelevator.com>
2020-02-03 15:28:20 -0500

    pushing fileIO lecture

---
+++ module-1/17_FileIO_Reading_in/student-lecture/java/src/main/java/com/techelevator/exceptions/ExceptionsLecture.java
@@ -0,0 +1,56 @@
+package com.techelevator.exceptions;
+
+import java.util.Scanner;
+
+public class ExceptionsLecture {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+
+	}
+	
+
+	// Utility functions:
+	
+	private static double calculateHotelRoomCharges(int nights, int numberOfGuests) {
+		final double EXTRA_GUEST_CHARGE = 20;
+		final double ROOM_RATE = 85;
+		
+		/* The throw statements below demonstrate how to throw a new Exception. */
+		if(nights < 1) {
+			throw new IllegalArgumentException("Minimum number of nights is 1");
+		} else if(numberOfGuests < 1) {
+			throw new IllegalArgumentException("Minimum number of guests is 1");
+		}
+		
+		int numberOfExtraGuests = 0;
+		if(numberOfGuests > 4) {
+			numberOfExtraGuests = numberOfGuests - 4;
+		}
+		
+		double dailyRate = ROOM_RATE + (EXTRA_GUEST_CHARGE * numberOfExtraGuests);
+		return dailyRate * nights;
+	}
+	
+	
+	public static double withdraw(double currentBalance, double amountToWithdraw) throws OverdraftException {
+		double newBalance;
+		if(amountToWithdraw <= currentBalance) {
+			newBalance = currentBalance - amountToWithdraw;
+		} else {
+			throw new OverdraftException("The requested withdrawal amount is greater than the current balance", Math.abs(currentBalance - amountToWithdraw));
+		}
+		return newBalance;		
+	}
+
+	
+	private static void doSomethingDangerous() {
+		int[] numbers = new int[5];
+		for(int i = 0; i < 10; i++) {
+			numbers[i] = i;  // this line will throw an Exception once i reaches 5
+		}
+		System.out.println("Look Ma, no Exceptions!");  // This line will not execute because an Exception will be thrown inside the for loop
+	}
+
+}
