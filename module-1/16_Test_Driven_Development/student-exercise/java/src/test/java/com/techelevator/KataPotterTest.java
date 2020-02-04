9d2361188255f7ae4ef0f09bb687308476326069
Willis Koa <williskoa@yahoo.com>
2020-02-04 10:27:55 -0500

    Added test for set of 5 books

--- module-1/16_Test_Driven_Development/student-exercise/java/src/test/java/com/techelevator/KataPotterTest.java
+++ module-1/16_Test_Driven_Development/student-exercise/java/src/test/java/com/techelevator/KataPotterTest.java
@@ -41,4 +41,12 @@ public class KataPotterTest {
 		assertEquals(actual, expected, 0.0001);
 	}
 	
+	@Test
+	public void set_of_5_returns_30() {
+		double actual = 30.0;
+		int[] books = new int[] {1, 1, 1, 1, 1};
+		double expected = potter.getCost(books);
+		assertEquals(actual, expected, 0.0001);
+	}
+	
 }
