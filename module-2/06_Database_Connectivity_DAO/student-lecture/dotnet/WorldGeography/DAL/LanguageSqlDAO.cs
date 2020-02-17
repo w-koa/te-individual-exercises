8edea18df70242cbc2a6ba12895b2e5b290f3570
Margaret Green <margaret@techelevator.com>
2020-02-17 08:42:38 -0500

    pushing today's lecture

---
+++ module-2/06_Database_Connectivity_DAO/student-lecture/dotnet/WorldGeography/DAL/LanguageSqlDAO.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WorldGeography.Models;
+
+namespace WorldGeography.DAL
+{
+    public class LanguageSqlDAO : ILanguageDAO
+    {
+        private string connectionString;
+
+        /// <summary>
+        /// Creates a sql based language dao.
+        /// </summary>
+        /// <param name="databaseConnectionString"></param>
+        public LanguageSqlDAO(string databaseConnectionString)
+        {
+            connectionString = databaseConnectionString;
+        }
+
+        public IList<Language> GetLanguages(string countryCode)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool AddNewLanguage(Language newLanguage)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool RemoveLanguage(Language deadLanguage)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
