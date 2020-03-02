834ed2bbec5c97f2511fd0f45f13f5ea34cb0d66
Andy <andy@techelevator.com>
2020-03-01 20:00:46 -0500

    Pushing 03-06

---
+++ module-3/06-Forms-and-Controllers-HTTP-GET/student-lecture/java/m3-controllers-part1-lecture-fbn/src/main/java/com/techelevator/fbn/controller/PaymentCalculatorController.java
@@ -0,0 +1,34 @@
+package com.techelevator.fbn.controller;
+
+import java.math.BigDecimal;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.techelevator.fbn.model.MortgageLoanEstimate;
+
+/* The @Controller annotation will make the Spring container aware
+ * of this controller as a managed bean. Controllers in Spring Web
+ * MVC are just plain old Java objects that are annotated to indicate
+ * that they are an MVC controller
+ *
+ * The @Controller annotation indicates to the Spring framework
+ * that this class should be treated as a controller
+ *
+ * The <component-scan> element in springmvc-servlet.xml causes
+ * Spring to look at all of the classes in the package specified
+ * and look for classes with this annotation. This controller will
+ * be found during the Spring component scan during application
+ * startup.
+ *
+ * Controllers (and pretty much everything else in Spring) can also be
+ * configured using XML.  However, annotations have become the
+ * suggested way of wiring together a Spring application. */
+public class PaymentCalculatorController {
+
+
+}
