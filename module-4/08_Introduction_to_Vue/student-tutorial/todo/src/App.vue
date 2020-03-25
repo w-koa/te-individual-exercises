6b91973e2fcb8b8ca692d8bda61bd2178daf6785
Willis Koa <williskoa@yahoo.com>
2020-03-25 15:46:49 -0400

    vue tutorial

--- module-4/08_Introduction_to_Vue/student-tutorial/todo/src/App.vue
+++ module-4/08_Introduction_to_Vue/student-tutorial/todo/src/App.vue
@@ -1,16 +1,16 @@
 <template>
   <div id="todo-app">
-    
+    <todo-list></todo-list>
   </div>
 </template>
 
 <script>
 
-
+import TodoList from './components/TodoList';
 export default {
   name: 'app',
   components: {
-    
+    TodoList
   }
 }
 </script>
