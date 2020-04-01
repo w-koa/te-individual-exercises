3e99668c47b6f68e9395053cb94b55f55b563f08
Andy <andy@techelevator.com>
2020-04-01 08:10:57 -0400

    Pushing 04-11

---
+++ module-4/11_Vue_Nested_Components/student-lecture/all-product-reviews/src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="app">
+    <search-bar/>
+    <product-review/>
+  </div>
+</template>
+
+<script>
+import ProductReview from './components/ProductReview.vue';
+import SearchBar from './components/SearchBar.vue';
+
+export default {
+  name: 'app',
+  components: {
+    ProductReview,
+    SearchBar
+  },
+  data() {
+    return {
+      filter: ''
+    };
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+#app div.main {
+  display: inline-block;
+  width: 45%;
+  padding: 1em;
+  border: 1px black solid;
+  border-radius: 5px;
+  margin: 0 1em;
+  vertical-align: top;
+}
+</style>
---
+++ module-4/11_Vue_Nested_Components/tutorial-final/todo-lists/src/components/TodoSearch.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="search">
+    <input type="text" id="search" name="search" v-on:keyup="filterTasks" placeholder="Search Todo's..."/>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    filterTasks() {
+        const query = document.getElementById('search').value;
+        this.$emit('filter-tasks', query);
+    }
+  }
+}
+</script>
+
+<style>
+#search {
+  width:100%;
+  font-size:24px;
+  font-family: 'Roboto Condensed', sans-serif;
+  margin-top:50px;
+  padding:10px;
+}
+</style>
