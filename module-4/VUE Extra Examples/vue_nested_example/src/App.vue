f5792e68de77159a148f01b8ed497fc1e06b85ad
Andy <andy@techelevator.com>
2020-04-03 13:20:43 -0400

    Pushing 04-13 lecture final and VUE extra examples

---
+++ module-4/VUE Extra Examples/vue_nested_example/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <!-- 1. I am passing data from parent to child
+    Child has a prop called msgFromParent which is sent
+    to the Child by adding the property and giving it a 
+    value:
+     -->
+
+    <!-- 6. The Child emits an event called childEvent, in
+    response to this emission the parent calls a method called
+    listenToChild
+     -->
+    <Child msgFromParent='Hey you' v-on:childEvent='listenToChild'/>
+
+
+  </div>
+</template>
+
+<script>
+import Child from './components/Child.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Child
+  },
+  methods: {
+
+    // 7. listenToChild is called in response to the child's emission.
+    // If I want to access the value attached to the emmission, I
+    // can simply add a parameter to the method like so:
+
+    listenToChild(waaaaa) {
+      window.alert(waaaaa);
+    }
+  }
+}
+</script>
---
+++ module-4/VUE Extra Examples/vue_nested_example/src/components/Child.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="hello">
+
+    <!--3. Simply print the prop value-->
+    Message from Parent Component: {{msgFromParent}}
+    <br>
+    <!--4. Pressing this button calls the sendMsgToParent method -->
+    <button v-on:click='sendMsgToParent'>Reply To Parent</button>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Child',
+
+  // 2. In this example, the parent passes a message to
+  // the child by defining a message while bringing in 
+  // the Child component. See step 1.
+  props: {
+    msgFromParent: String
+  },
+  methods :{
+
+    //5. this method emits an event back to the parent called childEvent.
+    sendMsgToParent() {
+      this.$emit('childEvent', 'what?');
+    }
+  }
+}
+</script>
